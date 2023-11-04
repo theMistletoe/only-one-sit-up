d5d07758eeaf1a688f0cf8f3161855ff35c033d5
theMistletoe <katachiwotodomete@gmail.com>
2023-11-04 22:52:17 +0900

    add confetti

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:vibration/vibration.dart';
+import 'package:confetti/confetti.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,47 +38,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<double>? _accelerometerValues;
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  late ConfettiController _confettiController;
 
   // Variables for counting sit-ups
   int _sitUpCount = 0;
   bool _isUserUp = false;
   bool _isInitialPosition = true;
-  final double _sitUpThreshold = 9.81 / 2; // Adjust the threshold accordingly
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title ?? 'Sit-Up Counter'),
-        elevation: 4,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Sit-ups: $_sitUpCount',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            const SizedBox(height: 20),
-            // Text('Accelerometer: ${_accelerometerValues?.map((v) => v.toStringAsFixed(1)).join(', ')}'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    for (final subscription in _streamSubscriptions) {
-      subscription.cancel();
-    }
-    super.dispose();
-  }
 
   @override
   void initState() {
     super.initState();
+    _confettiController =
+        ConfettiController(duration: const Duration(seconds: 1));
     _streamSubscriptions.add(
       accelerometerEvents.listen(
         (AccelerometerEvent event) {
@@ -88,6 +61,9 @@ class _MyHomePageState extends State<MyHomePage> {
             if (event.y > 9.0 && !_isUserUp && !_isInitialPosition) {
               _isUserUp = true;
               _sitUpCount++;
+              if (_sitUpCount > 0) {
+                _confettiController.play();
+              }
               // Check if the device can vibrate and provide haptic feedback
               Vibration.hasVibrator().then((bool? hasVibrator) {
                 if (hasVibrator ?? false) {
@@ -117,5 +93,52 @@ class _MyHomePageState extends State<MyHomePage> {
         },
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? 'Sit-Up Counter'),
+        elevation: 4,
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sit-ups: $_sitUpCount',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                const SizedBox(height: 20),
+                // Text('Accelerometer: ${_accelerometerValues?.map((double v) => v.toStringAsFixed(1)).join(', ')}'),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirection: -pi / 2, // bottom to top
+              maxBlastForce: 5, // set a maximum blast force
+              minBlastForce: 2, // set a minimum blast force
+              numberOfParticles: 50, // number of particles to emit
+              gravity: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+    _confettiController.dispose();
+    super.dispose();
   }
 }
