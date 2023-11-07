3db0aa43ee96afdbc72f4a2d3fb0a86a2acce54c
theMistletoe <katachiwotodomete@gmail.com>
2023-11-07 20:32:26 +0900

    change storage from shared_preferences to sqlflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
-import 'dart:convert'; // For json encoding/decoding
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:vibration/vibration.dart';
 import 'package:confetti/confetti.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,8 +32,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -40,97 +42,83 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
   late ConfettiController _confettiController;
-
-  // Variables for counting sit-ups
   int _sitUpCount = 0;
   bool _isUserUp = false;
   bool _isInitialPosition = true;
-  Map<String, int> _sitUpLog = {}; // Store date and count
-
-  bool shouldShowCongrats() {
-    return _sitUpCount >= 1;
-  }
+  Map<String, int> _sitUpLog = {};
 
   @override
   void initState() {
     super.initState();
-    // ... Other init code
-    loadSitUpLog();
     _confettiController =
         ConfettiController(duration: const Duration(seconds: 1));
-    _streamSubscriptions.add(
-      accelerometerEvents.listen(
-        (AccelerometerEvent event) {
-          // Use setState to rebuild the UI with new values
-          setState(() {
-            // Check if the device has been tilted enough to count as a sit-up
-            if (event.y > 9.0 && !_isUserUp && !_isInitialPosition) {
-              _isUserUp = true;
-              _sitUpCount++;
-              saveSitUpCount();
-              if (_sitUpCount > 0) {
-                _confettiController.play();
-              }
-              // Check if the device can vibrate and provide haptic feedback
-              Vibration.hasVibrator().then((bool? hasVibrator) {
-                if (hasVibrator ?? false) {
-                  Vibration.vibrate(
-                      duration: 10); // Vibrate for 100 milliseconds
-                }
-              });
-            } else if (event.y < 1.0 &&
-                event.z < 0.0 &&
-                (_isUserUp || _isInitialPosition)) {
-              // This condition checks if the user has returned to the initial position
-              _isUserUp = false;
-              _isInitialPosition = false;
+    loadSitUpLog();
+    _streamSubscriptions.add(accelerometerEvents.listen(
+      (AccelerometerEvent event) {
+        setState(() {
+          if (event.y > 9.0 && !_isUserUp && !_isInitialPosition) {
+            _isUserUp = true;
+            _sitUpCount++;
+            saveSitUpCount();
+            if (_sitUpCount > 0) {
+              _confettiController.play();
             }
-          });
-        },
-        onError: (e) {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return const AlertDialog(
-                title: Text("Error"),
-                content: Text("Accelerometer events error"),
-              );
-            },
-          );
-        },
-      ),
-    );
+            Vibration.hasVibrator().then((bool? hasVibrator) {
+              if (hasVibrator ?? false) {
+                Vibration.vibrate(duration: 10);
+              }
+            });
+          } else if (event.y < 1.0 &&
+              event.z < 0.0 &&
+              (_isUserUp || _isInitialPosition)) {
+            _isUserUp = false;
+            _isInitialPosition = false;
+          }
+        });
+      },
+      onError: (e) {
+        if (!mounted) return; // Make sure the context is still valid
+        showDialog(
+          context:
+              context, // Correctly references the BuildContext from the build method
+          builder: (BuildContext dialogContext) {
+            // Use a different name here to avoid confusion
+            return const AlertDialog(
+              title: Text("Error"),
+              content: Text("Accelerometer events error"),
+            );
+          },
+        );
+      },
+    ));
   }
 
   void loadSitUpLog() async {
-    final prefs = await SharedPreferences.getInstance();
+    final allRows = await DatabaseHelper.instance.queryAllRows();
     setState(() {
-      _sitUpLog = Map<String, int>.from(
-          json.decode(prefs.getString('sitUpLog') ?? '{}'));
+      _sitUpLog = {for (var row in allRows) row['date']: row['count']};
     });
   }
 
   void saveSitUpCount() async {
-    final prefs = await SharedPreferences.getInstance();
     final today = getTodayDate();
+    final count = (_sitUpLog[today] ?? 0) + 1;
     setState(() {
-      _sitUpLog[today] = (_sitUpLog[today] ?? 0) + 1; // Increment today's count
-      prefs.setString('sitUpLog', json.encode(_sitUpLog)); // Save it
+      _sitUpLog[today] = count;
     });
+    await DatabaseHelper.instance.insert(today, count);
   }
 
   String getTodayDate() {
-    return DateTime.now()
-        .toIso8601String()
-        .substring(0, 10); // YYYY-MM-DD format
+    return DateTime.now().toIso8601String().substring(0, 10);
   }
 
-  // Additional Widget to display the log
   Widget sitUpLogWidget() {
     return ListView.builder(
-      shrinkWrap: true, // Use only the space needed for children
+      shrinkWrap: true,
       itemCount: _sitUpLog.keys.length,
-      itemBuilder: (context, index) {
+      itemBuilder: (BuildContext ctx, int index) {
+        // Renamed context to ctx to avoid shadowing
         String date = _sitUpLog.keys.elementAt(index);
         return ListTile(
           title: Text(date),
@@ -144,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title ?? 'Sit-Up Counter'),
+        title: Text(widget.title),
         elevation: 4,
       ),
       body: Stack(
@@ -158,24 +146,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
                 const SizedBox(height: 20),
-                // Use the shouldShowCongrats function to determine visibility of the "Well done!" text
-                Visibility(
-                  visible: shouldShowCongrats(),
-                  child: const Text(
+                if (_sitUpCount >= 1)
+                  const Text(
                     'Well done!',
                     style: TextStyle(
                       fontSize: 36,
                       fontWeight: FontWeight.bold,
-                      color: Colors.green, // Choose a color that stands out
+                      color: Colors.green,
                     ),
                   ),
-                ),
-                // Add more space between the "Well done!" text and the counter if needed
-                if (shouldShowCongrats()) const SizedBox(height: 20),
-                // The rest of your widgets...
-                Expanded(
-                    child:
-                        sitUpLogWidget()), // Make sure it takes the needed space
+                if (_sitUpCount >= 1) const SizedBox(height: 20),
+                Expanded(child: sitUpLogWidget()),
               ],
             ),
           ),
@@ -183,10 +164,10 @@ class _MyHomePageState extends State<MyHomePage> {
             alignment: Alignment.topCenter,
             child: ConfettiWidget(
               confettiController: _confettiController,
-              blastDirection: pi / 2, // bottom to top
-              maxBlastForce: 5, // set a maximum blast force
-              minBlastForce: 2, // set a minimum blast force
-              numberOfParticles: 50, // number of particles to emit
+              blastDirection: pi / 2,
+              maxBlastForce: 5,
+              minBlastForce: 2,
+              numberOfParticles: 50,
               gravity: 1,
             ),
           ),
@@ -202,5 +183,49 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     _confettiController.dispose();
     super.dispose();
+  }
+}
+
+class DatabaseHelper {
+  static const _databaseName = "SitUpDatabase.db";
+  static const _databaseVersion = 1;
+  static const table = 'sit_up_table';
+  static const columnDate = 'date';
+  static const columnCount = 'count';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = p.join(documentsDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $table (
+        $columnDate TEXT PRIMARY KEY,
+        $columnCount INTEGER NOT NULL
+      )
+    ''');
+  }
+
+  Future<void> insert(String date, int count) async {
+    Database db = await instance.database;
+    await db.insert(
+      table,
+      {columnDate: date, columnCount: count},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database db = await instance.database;
+    return await db.query(table);
   }
 }
